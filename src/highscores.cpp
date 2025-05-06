e0c4ab5c75995864319c47f5d77ca95f34c14e08
Carsten Holtkamp <caholtk@gmail.com>
2025-05-06 03:07:39 +0200

    up

--- src/highscores.cpp
+++ src/highscores.cpp
@@ -1,93 +1,83 @@
 class highScoreClass {
-  private:
     string name;
     textureManager texMgr;
     textureClass tex;
-  public:
-  
-    highScoreClass()
-    {
-      name="";
-      texMgr.load(useTheme("/gfx/highscore/entername.png",setting.gfxTheme), tex);
+
+public:
+    highScoreClass() {
+        name = "";
+        texMgr.load(useTheme("/gfx/highscore/entername.png", setting.gfxTheme), tex);
     }
-  
-    void draw()
-    {
 
-      glLoadIdentity();
-      glTranslatef(0,0,-3.0);
-      glColor4f(1,1,1,1);
- 
-      glBindTexture(GL_TEXTURE_2D, tex.prop.texture);
-      glBegin( GL_QUADS );
-        glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, 0.5f, 0.0f );
-        glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, 0.5f, 0.0f );
-        glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,-0.5f, 0.0f );
-        glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,-0.5f, 0.0f );
-      glEnd( );      
+    void draw() const {
+        glLoadIdentity();
+        glTranslatef(0, 0, -3.0);
+        glColor4f(1, 1, 1, 1);
+
+        glBindTexture(GL_TEXTURE_2D, tex.prop.texture);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0.0f, 0.0f);
+        glVertex3f(-1.0f, 0.5f, 0.0f);
+        glTexCoord2f(1.0f, 0.0f);
+        glVertex3f(1.0f, 0.5f, 0.0f);
+        glTexCoord2f(1.0f, 1.0f);
+        glVertex3f(1.0f, -0.5f, 0.0f);
+        glTexCoord2f(0.0f, 1.0f);
+        glVertex3f(-1.0f, -0.5f, 0.0f);
+        glEnd();
 
 
-      glColor4f(1,1,1,1);
-      glText->write(name, FONT_MENUHIGHSCORE, 1, 2.0, 0.0, 0.0);
-      
+        glColor4f(1, 1, 1, 1);
+        glText->write(name, FONT_MENUHIGHSCORE, 1, 2.0, 0.0, 0.0);
     }
-    
-    void type(SDL_Event e, menuClass &menu)
-    {
-      if(var.showHighScores == 1)
-      {
-        if(e.key.keysym.sym == SDLK_ESCAPE)
-        {
-          var.showHighScores = 0;
-          initNewGame();
-          menu.refreshHighScoreList();
-          var.menu = 7;
-          return;
-        }
-        
-        if(e.key.keysym.sym != SDLK_RETURN)
-        {
-            if(e.key.keysym.sym == SDLK_BACKSPACE)
-            {
-              if(name.length() > 0)
-                name.erase(name.length()-1);
-            } else {
-              name += e.key.keysym.sym;
+
+    void type(const SDL_Event &e, menuClass &menu) {
+        if (var.showHighScores == 1) {
+            if (e.key.keysym.sym == SDLK_ESCAPE) {
+                var.showHighScores = 0;
+                initNewGame();
+                menu.refreshHighScoreList();
+                var.menu = 7;
+                return;
             }
-        } else {
-          var.showHighScores = 0;
-          ofstream hsList;
-          hsList.open(privFile.highScoreFile.data(), ios::out | ios::app);
-          hsList << "[" << player.level+1 << "]" << player.score << "|" << name << endl;
-          hsList.close();
-          initNewGame();
-          menu.refreshHighScoreList();
-          resumeGame();
-          var.titleScreenShow=1;
+
+            if (e.key.keysym.sym != SDLK_RETURN) {
+                if (e.key.keysym.sym == SDLK_BACKSPACE) {
+                    if (name.length() > 0)
+                        name.erase(name.length() - 1);
+                } else {
+                    name += e.key.keysym.sym;
+                }
+            } else {
+                var.showHighScores = 0;
+                ofstream hsList;
+                hsList.open(privFile.highScoreFile.data(), ios::out | ios::app);
+                hsList << "[" << player.level + 1 << "]" << player.score << "|" << name << endl;
+                hsList.close();
+                initNewGame();
+                menu.refreshHighScoreList();
+                resumeGame();
+                var.titleScreenShow = true;
+            }
         }
-      }
     }
 
-    bool isHighScore()
-    {
-      int n;
-      int high=0;
-      struct score * slist = sortScores(&n);
-      for(int i = 0; i < n; i++)
-      {
-        if(player.score < slist[i].score)
-        {
-          high++;
+    static bool isHighScore() {
+        int n;
+        int high = 0;
+        const score *slist = sortScores(&n);
+        for (int i = 0; i < n; i++) {
+            if (player.score < slist[i].score) {
+                high++;
+            }
         }
-      }
-      
-      if(n>0)
-        delete[] slist;
-      
-      if(high < 20)
-      {
-        return(1);
-      }
-      return(0);
+
+        if (n > 0)
+            delete[] slist;
+
+        if (high < 20) {
+            return true;
+        }
+        return false;
     }
 };
