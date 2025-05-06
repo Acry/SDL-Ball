ac09504552c15e473f32379567ecff1111e1cf20
Carsten Holtkamp <caholtk@gmail.com>
2025-05-06 03:33:21 +0200

    up

--- src/display.cpp
+++ src/display.cpp
@@ -6,137 +6,120 @@
 
 using namespace std;
 
-bool displayClass::init(bool fullscreen, int displayToUseIn)
-{
-  int SDL_videomodeSettings = SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE;
+bool displayClass::init(const bool fullscreen, const int displayToUseIn) {
+    int SDL_videomodeSettings = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
 
-  //Save current resolution so it can be restored at exit
-  // Declare display mode structure to be filled in.
-  SDL_DisplayMode currentDisplayMode;
+    //Save the current resolution so it can be restored at exit
+    // Declare display mode structure to be filled in.
+    SDL_DisplayMode currentDisplayMode;
 
-  SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
 
-  // Get current display mode of all displays.
-  numOfDisplays = SDL_GetNumVideoDisplays();
-  SDL_Rect displayBounds[numOfDisplays]={0};
+    // Get the current display mode of all displays.
+    numOfDisplays = SDL_GetNumVideoDisplays();
+    SDL_Rect displayBounds[numOfDisplays] = {0};
 
-  for(int i = 0; i < numOfDisplays; ++i){
-
-    int should_be_zero = SDL_GetCurrentDisplayMode(i, &currentDisplayMode);
-
-    if(should_be_zero != 0)
-    {
-      // In case of error...
-      SDL_Log("Could not get display mode for video display #%d: %s", i, SDL_GetError());
+    for (int i = 0; i < numOfDisplays; ++i) {
+        if (const int should_be_zero = SDL_GetCurrentDisplayMode(i, &currentDisplayMode); should_be_zero != 0) {
+            // In case of error...
+            SDL_Log("Could not get display mode for video display #%d: %s", i, SDL_GetError());
+        } else {
+            // On success, print the current display mode.
+            SDL_GetDisplayBounds(i, &displayBounds[i]);
+            auto [x, y, w, h] = displayBounds[i];
+            SDL_Log("Display #%d: current display mode is %dx%dpx @ %dhz. %d %d %d %d",
+                    i, currentDisplayMode.w, currentDisplayMode.h, currentDisplayMode.refresh_rate,
+                    x, y, h, w);
+        }
     }
-    else
-    {
-      // On success, print the current display mode.
-      SDL_GetDisplayBounds( i, &displayBounds[i] );
-      SDL_Rect currentDisplayBounds = displayBounds[i];
-      SDL_Log("Display #%d: current display mode is %dx%dpx @ %dhz. %d %d %d %d",
-    		  i, currentDisplayMode.w, currentDisplayMode.h, currentDisplayMode.refresh_rate,
-			  currentDisplayBounds.x,currentDisplayBounds.y,currentDisplayBounds.h,currentDisplayBounds.w);
+
+    if (numOfDisplays > 0 && displayToUseIn < numOfDisplays) {
+        displayToUse = displayToUseIn;
+    } else {
+        displayToUse = 0;
     }
-  }
 
-  if((numOfDisplays > 0) && (displayToUseIn<numOfDisplays))
-  {
-	  displayToUse = displayToUseIn;
-  }
-  else
-  {
-	  displayToUse = 0;
-  }
+    currentW = displayBounds[displayToUse].w;
+    currentH = displayBounds[displayToUse].h;
 
-  currentW = displayBounds[displayToUse].w;
-  currentH = displayBounds[displayToUse].h;
-
-  //Initialize SDL
-  #ifndef NOSOUND
-  if(SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK) <0 )
-  #else
+    //Initialize SDL
+#ifndef NOSOUND
+    if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK) < 0)
+#else
   if(SDL_Init(SDL_INIT_TIMER|SDL_INIT_JOYSTICK) <0 )
-  #endif
-  {
-    printf("\nError: Unable to initialize SDL:%s\n", SDL_GetError());
-    return false;
-  }
+#endif
+    {
+        printf("\nError: Unable to initialize SDL:%s\n", SDL_GetError());
+        return false;
+    }
 
-//  if(fullscreen)
-//  {
-//    SDL_videomodeSettings |= SDL_WINDOW_FULLSCREEN;
-//  }
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 
-  sdlWindow = SDL_CreateWindow("SDL-Ball",
-		                    SDL_WINDOWPOS_CENTERED_DISPLAY(displayToUse),
-							SDL_WINDOWPOS_CENTERED_DISPLAY(displayToUse),
-							displayBounds[displayToUse].w, displayBounds[displayToUse].h,
-							SDL_videomodeSettings);
+    if (fullscreen) {
+        // SDL_videomodeSettings |= SDL_WINDOW_FULLSCREEN_DESKTOP;  // Für echten Vollbildmodus
+        // Alternativ:
+        SDL_videomodeSettings |= SDL_WINDOW_FULLSCREEN;
+    }
 
-  glcontext = SDL_GL_CreateContext(sdlWindow);
+    sdlWindow = SDL_CreateWindow("SDL-Ball",
+                                 SDL_WINDOWPOS_CENTERED_DISPLAY(displayToUse),
+                                 SDL_WINDOWPOS_CENTERED_DISPLAY(displayToUse),
+                                 displayBounds[displayToUse].w, displayBounds[displayToUse].h,
+                                 SDL_videomodeSettings);
 
-  resize(currentW, currentH);
+    glcontext = SDL_GL_CreateContext(sdlWindow);
 
-  if( (sdlWindow == NULL) ||  (glcontext == NULL))
-  {
-    SDL_Log("Error:%s", SDL_GetError());
-	return false;
-  }
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    if ((sdlWindow == nullptr) || (glcontext == nullptr)) {
+        SDL_Log("Error:%s", SDL_GetError());
+        return false;
+    }
+    int maj, min;
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &maj);
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &min);
+    SDL_Log("=== Active OpenGL Context ===");
+    SDL_Log("Context Version: %d.%d", maj, min);
 
-	return true;
+
+    int doubleBuffered = 0;
+    SDL_GL_GetAttribute(SDL_GL_DOUBLEBUFFER, &doubleBuffered);
+    SDL_Log("Double buffering %s", doubleBuffered ? "aktiviert" : "deaktiviert");
+    resize(currentW, currentH);
+    return true;
 }
 
-bool displayClass::updateForMenu()
-{
-	SDL_Log("displayClass::updateForMenu() called");
-	return 1;
+bool displayClass::updateForMenu() {
+    return true;
 }
 
-void displayClass::resize(int width, int height )
-{
-	SDL_Log("displayClass::resize() called");
+void displayClass::resize(const int width, const int height) {
+    if (height == 0)
+       return;
 
-	/* Height / width ration */
-	GLfloat ratio;
+    currentW = width;
+    currentH = height;
 
-	/* Protect against a divide by zero */
-	if ( height == 0 )
-		height = 1;
+    const GLfloat ratio = static_cast<GLfloat>(width) / static_cast<GLfloat>(height);
+    glunits_per_xpixel = 2.485281374 * ratio / currentW;
+    glunits_per_ypixel = 2.485281374 / currentH;
 
-	currentW = width;
-	currentH = height;
+    glViewport(0, 0, width, height);
 
-	ratio = ( GLfloat )width / ( GLfloat )height;
-	glunits_per_xpixel = (2.485281374*ratio) / currentW;
-	glunits_per_ypixel = 2.485281374 / currentH;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
+    gluPerspective(45.0f, ratio, 0.1f, 10.0f);
 
-	/* Setup our viewport. */
-	glViewport( 0, 0, ( GLsizei )width, ( GLsizei )height );
+    glMatrixMode(GL_MODELVIEW);
 
-	/* change to the projection matrix and set our viewing volume. */
-	glMatrixMode( GL_PROJECTION );
-	glLoadIdentity( );
-
-	/* Set our perspective */
-	gluPerspective( 45.0f, ratio, 0.1f, 10.0f );
-
-	/* Make sure we're chaning the model view and not the projection */
-	glMatrixMode( GL_MODELVIEW );
-
-	/* Reset The View */
-	glLoadIdentity();
+    glLoadIdentity();
 }
 
-void displayClass::close()
-{
-	SDL_DestroyWindow(sdlWindow);
-	SDL_GL_DeleteContext(glcontext);
+void displayClass::close() {
+    SDL_DestroyWindow(sdlWindow);
+    SDL_GL_DeleteContext(glcontext);
     SDL_SetRelativeMouseMode(SDL_FALSE);
     SDL_Quit();
 }
--- src/display.hpp
+++ src/display.hpp
@@ -3,22 +3,24 @@
 
 #include <SDL2/SDL.h>
 
-class displayClass
-{
-private:
-	SDL_GLContext glcontext;
-	int displayToUse;	//!< The display index to use
-public:
-	SDL_Window *sdlWindow;
-	int currentW;
-	int currentH;
-	int numOfDisplays;
-	GLfloat glunits_per_xpixel, glunits_per_ypixel;
-	bool init(bool, int);
-	bool updateForMenu();
-	void resize(int, int);
-	void close();
-};
+class displayClass {
+    SDL_GLContext glcontext;
+    int displayToUse;
 
+public:
+    SDL_Window *sdlWindow;
+    int currentW;
+    int currentH;
+    int numOfDisplays;
+    GLfloat glunits_per_xpixel, glunits_per_ypixel;
+
+    bool init(bool, int);
+
+    static bool updateForMenu();
+
+    void resize(int, int);
+
+    void close();
+};
 
 #endif /* DISPLAY_HPP_ */
