b0deec010c2eecab4a9f81d88bf90b97f045fa67
Carsten Holtkamp <caholtk@gmail.com>
2025-05-08 14:44:22 +0200

    up

---
+++ src/text.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <epoxy/gl.h>
+
+// Jeder Index entspricht einer bestimmten Font-Konfiguration aus der Datei fonts.txt.
+#define FONT_MENU 0
+#define FONT_ANNOUNCE_GOOD 1
+#define FONT_ANNOUNCE_BAD 2
+#define FONT_HIGHSCORE 3
+#define FONT_MENUHIGHSCORE 4
+#define FONT_INTROHIGHSCORE 5
+#define FONT_INTRODESCRIPTION 6
+#define FONT_NUM 7
+
+extern std::string useTheme(const std::string &path, const std::string &theme);
+
+struct glCharInfo_struct {
+    GLfloat Xa, Ya, Xb, Yb, width;
+};
+
+struct glFontInfo_struct {
+    GLuint tex;
+    GLfloat height;
+    glCharInfo_struct ch[255];
+};
+
+class glTextClass {
+public:
+    glTextClass();
+    ~glTextClass();
+    GLfloat getHeight(int font) const;
+    void write(const std::string &text, int font, bool center, GLfloat scale, GLfloat x, GLfloat y) const;
+
+private:
+    void genFontTex(const std::string &TTFfontName, int fontSize, int font);
+    glFontInfo_struct fontInfo[FONT_NUM];
+};
