516a04852196308c7213482c92f89b63c03844a5
Carsten Holtkamp <caholtk@gmail.com>
2025-05-27 10:43:27 +0200

    up

--- src/Display.hpp
+++ src/Display.hpp
@@ -6,7 +6,9 @@
 class Display {
     int displayToUse = 0;
     SDL_GLContext glcontext = nullptr;
+
     [[nodiscard]] bool initOpenGL(unsigned int flags);
+
 public:
     SDL_Window *sdlWindow = nullptr;
     int currentW;
@@ -14,11 +16,17 @@ public:
     int numOfDisplays;
     GLfloat glunits_per_xpixel, glunits_per_ypixel;
     int viewportX, viewportY, viewportH, viewportW;
+
     Display(const int display, const int width, const int height, const bool fullscreen);
+
     GLfloat playfield_ratio;
     GLfloat window_ratio;
+
     static bool updateForMenu();
+
     [[nodiscard]] bool screenshot(const std::filesystem::path &pathName) const;
+
     void resize(int, int);
+
     ~Display();
 };
